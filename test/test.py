fa593880ba1fb0e72bcf1bd41831e399513273f6
Maxx Tessmer <mhtessmer@gmail.com>
2021-07-12 21:32:23 -0700

    converted basic tests into real tests

--- test/test.py
+++ test/test.py
@@ -1,21 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as  plt
 from PulseShape import Pulse
 
 
 def test_construct():
-
     profile = np.loadtxt('data/Transferfunction.dat').T
     myPulse = Pulse(0.150, 0.000625, np.pi, freq=[40, 120], type='sech/tanh', beta=10, profile=profile)
 
+    ans = np.genfromtxt("data/sechtanh.csv", delimiter=',')
+    ans = ans[:, 0] + 1j * ans[:, 1]
+    np.testing.assert_almost_equal(myPulse.IQ, ans)
 
-    ans = np.loadtxt('data/20210401_SechTanh_150ns.shp', delimiter=',')
 
-    plt.plot(myPulse.IQ.real)
-    plt.plot(ans[:, 0])
-    plt.show()
+def test_gaussian():
+    profile = np.loadtxt('data/Transferfunction.dat').T
 
-    plt.plot(myPulse.IQ.imag)
-    plt.plot(ans[:, 1])
-    plt.show()
+    myPulse = Pulse(pulse_time=0.060,
+                    time_step=0.000625,
+                    flip=np.pi,
+                    type='gaussian',
+                    profile=profile,
+                    trunc=0.1)
 
+    ans = np.genfromtxt("data/gaussian.csv", delimiter=',', dtype=complex,
+                        converters={0: lambda x: complex(x.decode('utf8').replace('i', 'j'))})
+    np.testing.assert_almost_equal(myPulse.IQ, ans)
