b713a3571e84b59e6efe97ca7b8fcfc1246c78d0
Maxx Tessmer <mhtessmer@gmail.com>
2021-07-19 22:24:34 -0700

    add tests for chirp pulses

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
+from scipy.integrate import cumtrapz
 from PulseShape import Pulse
 
 sigma2fwhm = 2.35482004503
@@ -14,6 +15,94 @@ def test_bwcomp():
     ans = ans[:, 0] + 1j * ans[:, 1]
     np.testing.assert_almost_equal(pulse.IQ, ans)
 
+
+def test_estimate_timestep():
+    pulse = Pulse(pulse_time=0.128,
+                  flip=np.pi,
+                  freq=[-50, 50],
+                  amp=20,
+                  type='quartersin/linear',
+                  trise=0.010,
+                  oversample_factor=10)
+    assert pulse.time_step == 1e-3
+
+
+def test_linear_chirp():
+    pulse = Pulse(pulse_time=0.064,
+                  time_step=0.0001,
+                  flip=np.pi/2,
+                  freq=[60, 180],
+                  type='rectangular/linear')
+
+    t0 = np.arange(0, pulse.pulse_time + pulse.time_step, pulse.time_step)
+
+    BW = pulse.freq[0] - pulse.freq[1]
+    Amp = np.sqrt(4 * np.log(2) * BW / pulse.pulse_time) / (2 * np.pi)
+    f = -(BW / 2) + (BW / pulse.pulse_time) * t0
+
+    phi = cumtrapz(f, t0, initial=0)
+    phi = phi + abs(min(phi))
+    print(t0.shape, phi.shape)
+    IQ0 = Amp * np.exp(2j * np.pi * (phi + np.mean(pulse.freq) * t0))
+
+    np.testing.assert_almost_equal(pulse.IQ, IQ0)
+
+
+def test_quartersin_chirp():
+    pulse = Pulse(pulse_time=0.128,
+                  flip=np.pi,
+                  freq=[-50, 50],
+                  amp=20,
+                  type='quartersin/linear',
+                  trise=0.010,
+                  oversample_factor=10)
+
+    BW = pulse.freq[1] - pulse.freq[0]
+    dt = 1 / (2 * pulse.oversample_factor * BW / 2)
+    dt = pulse.pulse_time / (np.rint(pulse.pulse_time / dt))
+    t0 = np.arange(0, pulse.pulse_time + dt, dt)
+
+    t_part = np.arange(0, pulse.trise + dt, dt)
+    A = np.ones(len(t0))
+    A[:len(t_part)] = np.sin((np.pi * t_part) / (2 * pulse.trise))
+    A[-len(t_part):] = A[len(t_part) - 1::-1]
+
+    f = -(BW / 2) + (BW / pulse.pulse_time) * t0
+    phi = cumtrapz(f, t0, initial=0)
+    phi = phi + abs(min(phi))
+
+    IQ0 = pulse.amp * A * np.exp(2j * np.pi * phi)
+
+    np.testing.assert_almost_equal(pulse.IQ, IQ0)
+    np.testing.assert_almost_equal(pulse.amp * A, pulse.amplitude_modulation)
+    np.testing.assert_almost_equal(f, pulse.frequency_modulation)
+
+def test_halfsin_chirp():
+    pulse = Pulse(pulse_time=0.128,
+                  flip=np.pi,
+                  freq=[-50, 50],
+                  amp=20,
+                  type='halfsin/linear',
+                  oversample_factor=10)
+
+    BW = pulse.freq[1] - pulse.freq[0];
+    dt = 1 / (2 * pulse.oversample_factor * BW / 2)
+    dt = pulse.pulse_time / (np.rint(pulse.pulse_time / dt))
+    t0 = np.arange(0, pulse.pulse_time + dt, dt)
+
+    A = np.sin(np.pi * t0 / pulse.pulse_time);
+
+    f = -(BW / 2) + (BW / pulse.pulse_time) * t0
+
+    phi = cumtrapz(f, t0, initial=0)
+    phi = phi + abs(min(phi))
+
+    IQ0 = pulse.amp * A * np.exp(2j * np.pi * phi)
+
+    np.testing.assert_almost_equal(pulse.IQ, IQ0)
+    np.testing.assert_almost_equal(pulse.amplitude_modulation, pulse.amp * A)
+    np.testing.assert_almost_equal(pulse.frequency_modulation, f)
+    np.testing.assert_almost_equal(pulse.IQ, IQ0)
 
 def test_gaussian_rd():
     profile = np.loadtxt('data/Transferfunction.dat').T
@@ -102,3 +191,10 @@ def test_gaussian2():
                   amp=1)
 
     np.testing.assert_almost_equal(pulse.IQ.real, IQ0)
+
+def test_save_bruker():
+    profile = np.loadtxt('data/Transferfunction.dat').T
+
+    pulse = Pulse(0.150, 0.000625, np.pi, freq=[40, 120], type='sech/tanh', beta=10, profile=profile)
+
+    pulse.save_bruker('data/istmp.shp')
