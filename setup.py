59f523bdc18e7507a150aadd72fc99a30890c34b
Maxx <mhtessmer@gmail.com>
2021-08-24 17:11:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='PulseShape',
     version='0.1.0',
     packages=['PulseShape'],
-    install_requires=['numpy>=1.19', 'scipy>=1.7', 'numba>=0.53'],
-    url='',
-    license='',
+    install_requires=['numpy>=1.19', 'scipy>=1.5', 'numba>=0.50'],
+    url='https://gitlab.com/mtessmer/PulseShape',
+    license='GNU GPLv2',
     author='Maxx Tessmer',
     author_email='mhtessmer@gmail.com',
     description='A Pulse shaping program for EPR!'
